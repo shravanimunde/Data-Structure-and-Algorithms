e97607bfaacf6f3aca041fd820d5c0a307d3186b
Shravani munde <shravanimunde07@gmail.com>
2026-07-11 21:40:46 +0530

    Refactor Productofdigits class to ProductOfDigits

--- BasicMath/Productofdigits.java
+++ BasicMath/Productofdigits.java
@@ -1,19 +1,39 @@
 // Write a Java program to find the product of all digits of a given number.
+// Program to find the product of all digits
+
 import java.util.*;
-class Productofdigits{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in); 
-        System.out.println("Enter the number");   
-        int number1=sc.nextInt();
-        int sum = 1;
-        int lastnumber = 0;
-        while(number1>0){
-            lastnumber = number1 % 10;
-            sum = sum * lastnumber;
-            number1 = number1 / 10;
 
-        }
-    System.out.println(sum);
+class ProductOfDigits {
+    public static void main(String[] args) {
 
+        // Create Scanner object
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number:");
+
+        // Read the number
+        int number = sc.nextInt();
+
+        // Product starts from 1 because multiplying by 0 always gives 0
+        int product = 1;
+
+        // Stores the last digit
+        int lastDigit = 0;
+
+        // Repeat until all digits are processed
+        while (number > 0) {
+
+            // Get the last digit
+            lastDigit = number % 10;
+
+            // Multiply the digit with the current product
+            product = product * lastDigit;
+
+            // Remove the last digit
+            number = number / 10;
         }
+
+        // Print the final product
+        System.out.println(product);
+    }
 }
