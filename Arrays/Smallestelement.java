f406636846fd638c69ede4b47d70da3c30aa9fcc
shravanimunde <shravanimunde07@gmail.com>
2026-07-19 00:42:51 +0530

    Smallest element in array

---
+++ Arrays/Smallestelement.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Smallestelement{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array");
+        int size = sc.nextInt();
+        //creating array
+        int arr[] = new int[size];
+        //Taking input 
+        for(int i =0; i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        //Asuming the first element is the samllest element
+        int smallest = arr[0];
+        for(int i = 0 ; i<size;i++){
+            if(arr[i]<smallest){
+                smallest = arr[i];
+
+            }
+        }
+    System.out.print(smallest);
+
+
+    }
+}
